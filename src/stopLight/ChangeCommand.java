f5e85913bf1e181ef9b41a06890423f12aa19495
VK Vusuwandla <55940072+VamspireHallo@users.noreply.github.com>
2024-03-16 20:24:32 -0700

    Update ChangeCommand.java
    will have conflicts with mvc

--- src/stopLight/ChangeCommand.java
+++ src/stopLight/ChangeCommand.java
@@ -1,18 +1,16 @@
 package stopLight;
 
-import mvc.Command;
-import mvc.Model;
+import mvc.*;
 
 public class ChangeCommand extends Command {
+
 	public ChangeCommand(Model model) {
 		super(model);
 	}
 
-	public void execute() throws Exception { 
-		if (!(model instanceof Stoplight)) {
-			throw new Exception("Model must instantiate Stoplight");
-	     }
+	public void execute() {
 		Stoplight light = (Stoplight)model;
 		light.change();
 	}
+
 }
