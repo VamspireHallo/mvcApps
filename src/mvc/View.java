2521cce179ae3f72802afc201df84f6813e01e28
VK Vusuwandla <55940072+VamspireHallo@users.noreply.github.com>
2024-03-16 21:26:38 -0700

    Update View.java

--- src/mvc/View.java
+++ src/mvc/View.java
@@ -3,22 +3,35 @@ package mvc;
 import javax.swing.*;
 import java.awt.*;
 
-public class View extends JPanel {
-
+public class View extends JPanel implements Subscriber {
     protected Model model;
 
     public View(Model model) {
         this.model = model;
+        model.subscribe(this);
+    }
+
+    public void setModel(Model model) {
+        this.model.unsubscribe(this);
+        this.model = model;
+        model.subscribe(this);
+        repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         // Draw the view based on the model's data
-        // This method is called automatically whenever the panel needs to be repainted
+        // For simplicity, let's just draw a text indicating the unsaved changes
+        if (model.getUnsavedChanges()) {
+            g.setColor(Color.RED);
+            g.drawString("Unsaved Changes", 10, 20);
+        }
+    }
 
-        // Example drawing code:
-        g.setColor(Color.RED);
-        g.fillRect(50, 50, 100, 100); // Draw a red rectangle at (50, 50) with width 100 and height 100
+    @Override
+    public void update() {
+        // Called when the model notifies of changes
+        repaint();
     }
 }
