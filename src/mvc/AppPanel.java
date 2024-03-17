bc4ac58a73ed9cdfaf8c0a268cadf3e480a9cc40
VK Vusuwandla <55940072+VamspireHallo@users.noreply.github.com>
2024-03-16 20:58:39 -0700

    Update AppPanel.java

--- src/mvc/AppPanel.java
+++ src/mvc/AppPanel.java
@@ -1,71 +1,97 @@
 package mvc;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class AppPanel extends JPanel {
+public class AppPanel extends JPanel implements Subscriber, ActionListener  {
 
-    private AppFactory factory;
-    private View view;
     protected Model model;
-    private List<Command> editCommands;
-    protected ControlPanel controlPanel;
+    protected AppFactory factory;
+    protected View view;
+    protected JPanel controlPanel;
+    private JFrame frame;
+    public static int FRAME_WIDTH = 500;
+    public static int FRAME_HEIGHT = 300;
 
     public AppPanel(AppFactory factory) {
-        this.factory = factory;
-        this.model = factory.createModel();
-        this.view = factory.createView(model);
-        this.editCommands = factory.createEditCommands(model);
 
-        // Set layout for the AppPanel
-        setLayout(new BorderLayout());
+        // initialize fields here
 
-        // Add the view to the center of the AppPanel
-        add(view, BorderLayout.CENTER);
-
-        // Create and add the control panel to the south of the AppPanel
-        controlPanel = new ControlPanel(editCommands);
-        add(controlPanel, BorderLayout.SOUTH);
+        frame = new SafeFrame();
+        Container cp = frame.getContentPane();
+        cp.add(this);
+        frame.setJMenuBar(createMenuBar());
+        frame.setTitle(factory.getTitle());
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
     }
 
-    public void display() {
-        JFrame frame = new JFrame("MVC Application");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(this);
-        frame.pack();
-        frame.setVisible(true);
+    public void display() { frame.setVisible(true); }
+
+    public void update() {  /* override in extensions if needed */ }
+
+    public Model getModel() { return model; }
+
+    // called by file/open and file/new
+    public void setModel(Model newModel) {
+        this.model.unsubscribe(this);
+        this.model = newModel;
+        this.model.subscribe(this);
+        // view must also unsubscribe then resubscribe:
+        view.setModel(this.model);
+        model.changed();
     }
 
-    // Getter method for the controlPanel
-    public ControlPanel getControlPanel() {
-        return controlPanel;
+    protected JMenuBar createMenuBar() {
+        JMenuBar result = new JMenuBar();
+        // add file, edit, and help menus
+        JMenu fileMenu =
+                Utilities.makeMenu("File", new String[] {"New",  "Save", "SaveAs", "Open", "Quit"}, this);
+        result.add(fileMenu);
+
+        JMenu editMenu =
+                Utilities.makeMenu("Edit", factory.getEditCommands(), this);
+        result.add(editMenu);
+
+        JMenu helpMenu =
+                Utilities.makeMenu("Help", new String[] {"About", "Help"}, this);
+        result.add(helpMenu);
+
+        return result;
     }
 
-    public class ControlPanel extends JPanel {
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            String cmmd = ae.getActionCommand();
 
-        public ControlPanel(List<Command> editCommands) {
-            // Set layout for the control panel
-            setLayout(new FlowLayout());
-
-            // Create buttons for each edit command
-            for (Command command : editCommands) {
-                JButton button = new JButton(command.getClass().getSimpleName());
-                button.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        try {
-                            command.execute();
-                        } catch (Exception ex) {
-                            // Handle exceptions
-                            Utilities.error(ex);
-                        }
-                    }
-                });
-                add(button);
+            if (cmmd.equals("Save")) {
+                Utilities.save(model, false);
+            } else if (cmmd.equals("SaveAs")) {
+                Utilities.save(model, true);
+            } else if (cmmd.equals("Open")) {
+                Model newModel = Utilities.open(model);
+                if (newModel != null) setModel(newModel);
+            } else if (cmmd.equals("New")) {
+                Utilities.saveChanges(model);
+                setModel(factory.makeModel());
+                // needed cuz setModel sets to true:
+                model.setUnsavedChanges(false);
+            } else if (cmmd.equals("Quit")) {
+                Utilities.saveChanges(model);
+                System.exit(0);
+            } else if (cmmd.equals("About")) {
+                Utilities.inform(factory.about());
+            } else if (cmmd.equals("Help")) {
+                Utilities.inform(factory.getHelp());
+            } else { // must be from Edit menu
+                //???
             }
+        } catch (Exception e) {
+            handleException(e);
         }
+    }
+
+    protected void handleException(Exception e) {
+        Utilities.error(e);
     }
 }
