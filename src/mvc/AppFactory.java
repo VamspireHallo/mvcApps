5906644250b36acc5ce8a5802d611a85ffe30a3f
VK Vusuwandla <55940072+VamspireHallo@users.noreply.github.com>
2024-03-16 21:25:21 -0700

    Update AppFactory.java
    adding other methods

--- src/mvc/AppFactory.java
+++ src/mvc/AppFactory.java
@@ -4,11 +4,23 @@ import java.util.List;
 
 public interface AppFactory {
     // Method to create a model instance
-    Model makeModel();
+    public Model makeModel();
 
-    // Method to create a view instance
-    View makeView(Model model);
+    // Create and return a new instance of the view, given the model
+    public View makeView(Model model);
 
-    // Method to create commands for the Edit menu
-    String[] getEditCommands();
+    // Get the edit commands available in the application
+    public String[] getEditCommands();
+
+    // Create and return a command object based on the command type and source
+    public Command makeEditCommand(Model model, String type, Object source);
+
+    // Get the title of the application
+    public String getTitle();
+
+    // Get the help information for the application
+    public String[] getHelp();
+
+    // Get information about the application
+    public String about();
 }
