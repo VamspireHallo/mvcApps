6eed04898948026bf3ede083b16a38a32e6aa057
VK Vusuwandla <vamseevkv@gmail.com>
2024-03-19 22:31:13 -0700

    Society implemented

--- src/Life/Society.java
+++ src/Life/Society.java
@@ -3,11 +3,28 @@ package Life;
 import CALab.Cell;
 import CALab.Grid;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Society extends Grid
 {
+    public static Set<Integer> rebirth = new HashSet<>();
+    public static Set<Integer> death = new HashSet<>();
+    public static int percentAlive = 50;
+
+    static {
+        rebirth.add(3);
+        death.add(0);
+        death.add(1);
+        death.add(4);
+        death.add(5);
+        death.add(6);
+        death.add(7);
+        death.add(8);
+    }
 
     @Override
     public Cell makeCell(boolean uniform) {
-        return null;
+        return new Agent();
     }
 }
