5f71a5efbcda0c9ced6933ad1b4403314900536d
VK Vusuwandla <vamseevkv@gmail.com>
2024-03-20 18:44:10 -0700

    Implementing Society into Agent

--- src/Life/Agent.java
+++ src/Life/Agent.java
@@ -4,11 +4,16 @@ import CALab.*;
 
 import java.awt.*;
 import java.util.Random;
+import java.util.Set;
 
 public class Agent extends Cell
 {
     int status = 0;
     int ambience = 8;
+
+    Set<Integer> isReborn = Society.rebirth;
+    Set<Integer> isDead = Society.death;
+
 
     @Override
     public void observe() { //updates the ambience
@@ -29,11 +34,11 @@ public class Agent extends Cell
     @Override
     public void update() // if cell is alive with 0 1 4 5 6 7 8 neighbors it becomes dead. if dead with 2 or 3 becomes alive
     {
-        if (((ambience <= 8 && ambience >= 4) || (ambience == 1) || ambience == 0) && status == 1 )
+        if (isDead.contains(ambience) && status == 1 )
         {
             status = 0;
         }
-        if ((ambience == 1 || ambience == 2) && status == 0 )
+        if (isReborn.contains(ambience) && status == 0 )
         {
             status = 1;
         }
@@ -45,12 +50,10 @@ public class Agent extends Cell
         if (status == 0)
         {
             status = 1;
-
         }
         else
         {
             status = 0;
-
         }
 
     }
